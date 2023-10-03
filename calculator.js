1cf800cc207c3a103de1d24559ab8637781e94ce
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-10-03 16:03:49 -0700

    Add calculator tests and functions

---
+++ calculator.js
@@ -0,0 +1,18 @@
+const calculator = {
+  sum(a, b) {
+    return Number(a) + Number(b);
+  },
+
+  subtract(a, b) {
+    return Number(a) - Number(b);
+  },
+
+  multiply(a, b) {
+    return Number(a) * Number(b);
+  },
+
+  divide(a, b) {
+    return Number(a) / Number(b);
+  },
+}
+export default calculator;
---
+++ calculator.test.js
@@ -0,0 +1,53 @@
+import calculator from './calculator';
+
+// Addition
+test('Add single digits', () => {
+  expect(calculator.sum(1, 2)).toBe(3);
+});
+
+test('Add multiple digits', () => {
+  expect(calculator.sum(563829, 6784949)).toBe(7348778);
+});
+
+test('Add with strings', () => {
+  expect(calculator.sum("1", 2)).toBe(3);
+});
+
+// Subtraction
+test('Subtract single digits', () => {
+  expect(calculator.subtract(2, 1)).toBe(1);
+});
+
+test('Subtract multiple digits', () => {
+  expect(calculator.subtract(5738487, 19384)).toBe(5719103);
+});
+
+test('Subtract with strings', () => {
+  expect(calculator.subtract("2", 1)).toBe(1);
+});
+
+// Multiplication
+test('Multiply single digits', () => {
+  expect(calculator.multiply(2, 3)).toBe(6);
+});
+
+test('Multiply multiple digits', () => {
+  expect(calculator.multiply(2958, 38194)).toBe(112977852);
+});
+
+test('Multiply with strings', () => {
+  expect(calculator.multiply("2", 3)).toBe(6);
+});
+
+// Division
+test('Divide single digits', () => {
+  expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test('Divide multiple digits', () => {
+  expect(calculator.divide(17295, 283)).toBeCloseTo(61.113);
+});
+
+test('Divide with strings', () => {
+  expect(calculator.divide("6", 2)).toBe(3);
+});
